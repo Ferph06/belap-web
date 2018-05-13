c6701292b04c7d0c651fdecc1a50adb52b10948b
Ferph06 <fernando.perez060198@gmail.com>
2018-05-13 13:19:37 -0500

    cambio de diseño

--- resources/js/auth.js
+++ resources/js/auth.js
@@ -1,30 +1,26 @@
 /**
  * Configuración de la autentificación con los proveedores de servicios
  */
-app.config(function ($authProvider,socialProvider) {
-    //socialProvider.setGoogleKey("408273263196-677ms8qe5lol30prqbgrkggg50k0o3k3.apps.googleusercontent.com");
-   
-  socialProvider.setFbKey({appId: "1689177774454452", apiVersion: "v2.4"})
+app.config(function ($authProvider) {
     $authProvider.httpInterceptor = function () {
             return true;
-        };
-        $authProvider.baseUrl =API.endPoint
+        },
     $authProvider.withCredentials = false;
     $authProvider.tokenRoot = null;
-        $authProvider.baseUrl =API.endPointProd
+    
     $authProvider.facebook({
         clientId: '1689177774454452',
         responseType: 'token'
     });
     $authProvider.google({
-        clientId: '633437360795-vmg89oq6089pal6vchv0muac641d6uum.apps.googleusercontent.com'
+        clientId: '408273263196-677ms8qe5lol30prqbgrkggg50k0o3k3.apps.googleusercontent.com'
     });
     // Facebook
     $authProvider.facebook({
         name: 'facebook',
         url: '/user/facebookApp',
         authorizationEndpoint: 'https://www.facebook.com/v2.5/dialog/oauth',
-        redirectUri: window.location.origin+'/',
+         
         requiredUrlParams: ['display', 'scope'],
         scope: ['email'],
         scopeDelimiter: ',',
@@ -41,9 +37,8 @@ app.config(function ($authProvider,socialProvider) {
         url: '/user/googleApp',
         authorizationEndpoint: 'https://accounts.google.com/o/oauth2/auth',
         requiredUrlParams: ['scope'],
-        redirectUri: window.location.origin,
         optionalUrlParams: ['display'],
-        scope: ['profile'],
+        scope: ['profile', 'email'],
         scopePrefix: 'openid',
         scopeDelimiter: ' ',
         display: 'popup',
--- resources/js/core.js
+++ resources/js/core.js
@@ -1,7 +1,11 @@
 const app = angular.module('belap', ['ui.router', 'ngAnimate', 'toastr', 'satellizer','socialLogin']);
-const API = {
+/*const API = {
     endPoint: 'http://localhost:1337/api/v1',
-    endPointProd: 'https://belap-dev.herokuapp.com/api/v1',
+    endPoint: 'https://belap-dev.herokuapp.com/api/v1',
+    sess_id: OpenPay.deviceData.setup()
+}*/
+const API = {
+    endPoint: 'https://belap-dev.herokuapp.com/api/v1',
     sess_id: OpenPay.deviceData.setup()
 }
 app.run(function ($rootScope,$state) {
